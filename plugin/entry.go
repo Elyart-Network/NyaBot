10549c057992032dfe45313f99a5f90e50b7fdb5
Season Shi <seasonshi@elyart.dev>
2023-04-01 00:10:27 +0800

    Remove ThirdParty Plugins from main Branch

--- plugin/entry.go
+++ plugin/entry.go
@@ -1,15 +1,10 @@
 package plugin
 
 import (
-	"github.com/Elyart-Network/NyaBot/extend/plugin"
-	"github.com/Elyart-Network/NyaBot/plugin/FlarumGroup"
 	"github.com/gin-gonic/gin"
 )
 
-func GoCqEntry(ctx *gin.Context) {
-	go plugin.GoCqLoader(ctx, FlarumGroup.Entry)
-}
-
+func GoCqEntry(ctx *gin.Context)     {}
 func DiscordEntry(ctx *gin.Context)  {}
 func TelegramEntry(ctx *gin.Context) {}
 func SlackEntry(ctx *gin.Context)    {}
