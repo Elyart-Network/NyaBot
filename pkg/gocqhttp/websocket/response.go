b599f040b3550bfdb674276aef82ba3464649bbe
Season Shi <seasonshi@elyart.dev>
2023-04-09 01:23:31 +0800

    fix(Cq wsEncode): fixed when GoCqHttp send a meta_event, json Unmarshal failed.

--- pkg/gocqhttp/websocket/response.go
+++ pkg/gocqhttp/websocket/response.go
@@ -6,12 +6,13 @@ import (
 )
 
 type wsResponseData struct {
-	Status  string      `json:"status"`
-	RetCode int         `json:"retcode"`
-	Msg     string      `json:"msg"`
-	Wording string      `json:"wording"`
-	Echo    string      `json:"echo"`
-	Data    interface{} `json:"data"`
+	Status   string      `json:"status"`
+	RetCode  int         `json:"retcode"`
+	Msg      string      `json:"msg"`
+	Wording  string      `json:"wording"`
+	Echo     string      `json:"echo"`
+	Data     interface{} `json:"data"`
+	PostType string      `json:"post_type"`
 }
 
 var responseChan = make(chan wsResponseData)
@@ -19,6 +20,9 @@ var responseChan = make(chan wsResponseData)
 func wsResponseEncode(wsContext []byte) (resp wsResponseData, isResp bool, err error) {
 	// Encode Message to wsActionData.
 	err = json.Unmarshal(wsContext, &resp)
+	if resp.PostType == "meta_event" {
+		return resp, false, nil
+	}
 	if echo := resp.Echo; echo != "" && strings.HasPrefix(echo, "nyabot_") {
 		return resp, true, err
 	}
