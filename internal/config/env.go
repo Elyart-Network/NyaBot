ff9d87ff823b81c47772b13f4341a2320170e874
Season Shi <seasonshi@elyart.dev>
2023-05-05 01:21:59 +0800

    NYABOT-67 style(config): more elegant.

--- internal/config/env.go
+++ internal/config/env.go
@@ -5,6 +5,7 @@ import (
 	"github.com/joho/godotenv"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func GetEnv(Key string) string {
@@ -12,7 +13,13 @@ func GetEnv(Key string) string {
 	return os.Getenv(Key)
 }
 
-func SetEnvConf(Type string, ConfKey string, EnvKey string) {
+func SetEnvConf(Type string, ConfKey string) {
+	ConfSplit := strings.Split(ConfKey, ".")
+	var UpConfSplit []string
+	for _, v := range ConfSplit {
+		UpConfSplit = append(UpConfSplit, strings.ToUpper(v))
+	}
+	var EnvKey = strings.Join(UpConfSplit, "_")
 	env := GetEnv(EnvKey)
 	if env != "" {
 		switch Type {
@@ -36,33 +43,14 @@ func SetEnvConf(Type string, ConfKey string, EnvKey string) {
 }
 
 func EnvInit() {
-	SetEnvConf("string", "server.listen_port", "SERVER_LISTEN_PORT")
-	SetEnvConf("bool", "server.file_logger", "SERVER_FILE_LOGGER")
-	SetEnvConf("bool", "server.debug_mode", "SERVER_DEBUG_MODE")
-	SetEnvConf("bool", "search.enable", "SEARCH_ENABLED")
-	SetEnvConf("string", "search.host", "SEARCH_HOST")
-	SetEnvConf("int", "search.index_name", "SEARCH_INDEX_NAME")
-	SetEnvConf("string", "search.username", "SEARCH_USERNAME")
-	SetEnvConf("string", "search.password", "SEARCH_PASSWORD")
-	SetEnvConf("bool", "mirai.enable", "MIRAI_ENABLE")
-	SetEnvConf("bool", "logging.external", "LOGGING_EXTERNAL")
-	SetEnvConf("string", "logging.mongo_uri", "LOGGING_MONGO_URI")
-	SetEnvConf("string", "logging.username", "LOGGING_USERNAME")
-	SetEnvConf("string", "logging.password", "LOGGING_PASSWORD")
-	SetEnvConf("int", "logging.cache_num", "LOGGING_CACHE_NUM")
-	SetEnvConf("bool", "logging.internal_log", "LOGGING_INTERNAL_LOG")
-	SetEnvConf("bool", "cache.external", "CACHE_EXTERNAL")
-	SetEnvConf("string", "cache.host", "CACHE_HOST")
-	SetEnvConf("string", "cache.index_name", "CACHE_INDEX_NAME")
-	SetEnvConf("string", "cache.username", "CACHE_USERNAME")
-	SetEnvConf("string", "cache.password", "CACHE_PASSWORD")
-	SetEnvConf("string", "database.type", "DATABASE_TYPE")
-	SetEnvConf("string", "database.host", "DATABASE_HOST")
-	SetEnvConf("string", "database.name", "DATABASE_NAME")
-	SetEnvConf("string", "database.username", "DATABASE_USERNAME")
-	SetEnvConf("string", "database.password", "DATABASE_PASSWORD")
-	SetEnvConf("bool", "gocqhttp.enable", "GOCQHTTP_ENABLED")
-	SetEnvConf("string", "gocqhttp.host_url", "GOCQHTTP_HOST_URL")
-	SetEnvConf("int", "gocqhttp.delay", "GOCQHTTP_DELAY")
-	SetEnvConf("bool", "gocqhttp.enable_ws", "GOCQHTTP_ENABLE_WS")
+	var dict = map[string][]string{
+		"bool":   {"server.file_logger", "server.debug_mode", "search.enable", "mirai.enable", "logging.external", "logging.internal_log", "cache.external", "gocqhttp.enable", "gocqhttp.enable_ws"},
+		"int":    {"search.index_name", "logging.cache_num", "gocqhttp.delay"},
+		"string": {"search.host", "search.username", "search.password", "logging.mongo_uri", "logging.username", "logging.password", "cache.host", "cache.index_name", "cache.username", "cache.password", "database.type", "database.host", "database.name", "database.username", "database.password", "gocqhttp.host_url"},
+	}
+	for key, value := range dict {
+		for _, sub := range value {
+			SetEnvConf(key, sub)
+		}
+	}
 }
