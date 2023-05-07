34ccc4a8e0c28f69e50156f876dfa81b557feeb3
Season.Shi <seasonshi@elyart.dev>
2023-05-07 18:59:09 +0800

    NYABOT-49 feature(Logger): add debug and info logger

--- logger/debug.go
+++ logger/debug.go
@@ -1,1 +1,39 @@
 package logger
+
+import (
+	"github.com/Elyart-Network/NyaBot/config"
+	"log"
+	"strconv"
+)
+
+func CheckDebug() bool {
+	enable := config.Get("server.debug_mode").(bool)
+	return enable
+}
+
+func Debug(err error) {
+	if !CheckDebug() || err == nil {
+		return
+	}
+	go log.Println("[Logger] Debug: " + err.Error())
+}
+
+func DebugStr(err string) {
+	if !CheckDebug() || err == "" {
+		go log.Println("[Logger] Debug: empty string.")
+		return
+	}
+	go log.Println("[Logger] Debug: " + err)
+}
+
+func Debugf(format string, args ...any) {
+	if !CheckDebug() || format == "" {
+		go log.Println("[Logger] Debug: empty format.")
+		return
+	}
+	go func() {
+		for k, arg := range args {
+			log.Println("["+strconv.Itoa(k)+"] [Logger] Debug ("+format+"): ", arg)
+		}
+	}()
+}
--- logger/info.go
+++ logger/info.go
@@ -1,1 +1,33 @@
 package logger
+
+import (
+	"log"
+	"strconv"
+)
+
+func Info(err error) {
+	if err == nil {
+		return
+	}
+	go log.Println("[Logger] Info: " + err.Error())
+}
+
+func InfoStr(err string) {
+	if err == "" {
+		go log.Println("[Logger] Info: empty string.")
+		return
+	}
+	go log.Println("[Logger] Info: " + err)
+}
+
+func Infof(format string, args ...any) {
+	if format == "" {
+		go log.Println("[Logger] Info: empty format.")
+		return
+	}
+	go func() {
+		for k, arg := range args {
+			log.Println("["+strconv.Itoa(k)+"] [Logger] Info ("+format+"): ", arg)
+		}
+	}()
+}
