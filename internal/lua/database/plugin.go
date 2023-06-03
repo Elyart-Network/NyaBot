9e5bfc12d8259c7b3c11e3f8562f10e75966fdf5
BaiYi <seasonshi@elyart.dev>
2023-06-03 23:27:35 +0800

    feature(lua): Add Database Plugin functions.

--- internal/lua/database/plugin.go
+++ internal/lua/database/plugin.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"github.com/Elyart-Network/NyaBot/pkg/database"
 	lua "github.com/yuin/gopher-lua"
 	luar "layeh.com/gopher-luar"
 )
@@ -8,15 +9,15 @@ import (
 type DBPluginFunc struct{}
 
 func (p *DBPluginFunc) SetConfig(PName string, CKey string, CValue any) {
-
+	database.SetConfig(PName, "Lua", CKey, CValue)
 }
 
-func (p *DBPluginFunc) GetConfig() {
-
+func (p *DBPluginFunc) GetConfig(PName string) map[string]any {
+	return database.GetConfig(PName, "Lua")
 }
 
-func (p *DBPluginFunc) DelConfig() {
-
+func (p *DBPluginFunc) DelConfig(PName string, CKey string) {
+	database.DelConfig(PName, "Lua", CKey)
 }
 
 func DBPlugin(L *lua.LState) {
